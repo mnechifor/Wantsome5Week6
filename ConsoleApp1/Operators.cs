381f588e0c444deb9ef8d68f9487ad4b49ce8541
Mihai Nechifor <Mihai.Nechifor@endava.com>
2019-11-15 21:01:53 +0200

    upd

---
+++ ConsoleApp1/Operators.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Base
+    {
+    }
+
+    internal class Derived : Base
+    {
+    }
+
+    internal class Operators
+    {
+        private static void TestOperators()
+        {
+            object objBase = new Base();
+
+            if (objBase is Base)
+                Console.WriteLine("objBase is Base");
+            // Result: objBase is Base
+
+
+            if (!(objBase is Derived))
+                Console.WriteLine("objBase is not Derived");
+            // Result : objBase is not Derived
+
+
+            if (objBase is object)
+                Console.WriteLine("objBase is System.Object");
+            // Result : objBase is System.Object
+
+            var b = objBase as Base;
+            Console.WriteLine("b = {0}", b);
+            // Result: b = Base
+
+            var d = objBase as Derived;
+            if (d == null)
+                Console.WriteLine("d is null");
+            // Result: d is null
+            var o = objBase;
+            Console.WriteLine("o = {0}", o);
+            // Result: o = Base
+
+
+            var der = new Derived();
+            var bas = der as Base;
+
+            var aa = (Base) der;
+            Console.WriteLine("bas = {0}", bas);
+            // Result: bas = Derived
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int i = 1;
+
+            Add1(i);
+
+            Student s1 = new Student {Address = new Address {Line1 = "Iasi"}};
+
+            UpdateAddress(s1, "Bucuresti");
+
+            var newAddress = s1.Address.Line1;
+
+            var k = i;
+        }
+
+        public static void Add1(int nr)
+        {
+            nr++;
+
+        }
+
+        public static void UpdateAddress(Student student, string address)
+        {
+            student.Address.Line1 = address;
+        }
+
+        private static void PointExample()
+        {
+            CloneExample();
+
+            List<Point> list = new List<Point>();
+
+            var p1 = new Point {X = 4, Y = 7};
+            list.Add(p1);
+
+            var p2 = new Point {X = 1, Y = 7};
+            list.Add(p2);
+
+            list.Sort();
+        }
+
+        private static void CloneExample()
+        {
+            var s1 = new Student();
+            s1.Name = "Mihai";
+            s1.Address = new Address();
+            s1.Address.Line1 = "Iasi";
+
+
+
+
+            var s2 = new Student();
+            s2.Name = "Mihai";
+
+            if (s1 == s2)
+                Console.WriteLine("Egalitate");
+            else
+                Console.WriteLine("Diferite");
+
+
+            var s3 = s1.Clone();
+
+            s1.Address.Line1 = "Bucuresti";
+
+            var c = 5 is object;
+        }
+    }
+
+    class Point : IComparable<Point>
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int CompareTo(Point otherPoint)
+        {
+            if (this.X != otherPoint.X)
+            {
+                return (this.X - otherPoint.X);
+            }
+            if (this.Y != otherPoint.Y)
+            {
+                return (this.Y - otherPoint.Y);
+            }
+            return 0;
+        }
+    }
+
+    public class Address
+    {
+        public string Line1 { get; set; }
+    }
+
+    public class Student : ICloneable
+    {
+        public Address Address { get; set; }
+        public string Name { get; set; }
+
+        public List<int> Rooms { get; set; }
+
+        public int Age { get; set; }
+
+        public object Clone()
+        {
+            Student s1 = new Student();
+            s1.Name = this.Name;
+            s1.Age = this.Age;
+
+            s1.Address = new Address();
+            s1.Address.Line1 = this.Address.Line1;
+
+            //s1.Rooms = new List<int>();
+            //foreach (var room in Rooms)
+            //{
+            //    s1.Rooms.Add(new Room(room.Name));
+            //}
+
+            return s1;
+        }
+
+        public static bool operator ==(Student student1,
+            Student student2)
+        {
+            return Equals(student1, student2);
+        }
+
+        public static bool operator !=(Student student1,
+            Student student2)
+        {
+            return !Equals(student1, student2);
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() ^ Age.GetHashCode();
+        }
+
+        public override bool Equals(object param)
+        {
+            // If the cast is invalid, the result will be null
+            var student = param as Student;
+            // Check if we have valid not null Student object
+            if (student == null)
+                return false;
+            // Compare the reference type member fields
+            if (!Equals(Name, student.Name))
+                return false;
+            // Compare the value type member fields
+            if (Age != student.Age)
+                return false;
+
+            return true;
+        }
+    }
+}
